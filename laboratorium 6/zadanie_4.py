3270182f55647f03c33ed36dc251e347b176b1a6
GetArektt <92780875+GetArektt@users.noreply.github.com>
2021-11-16 10:17:45 +0100

    Update zadanie_4.py

--- laboratorium 6/zadanie_4.py
+++ laboratorium 6/zadanie_4.py
@@ -4,36 +4,60 @@
 #    -Należy sprawdzić przypadki użycia dla kilku wczytywanych liczb naturalnych.
 #    -Należy obsłużyć wyjątek wczytania danej niebędącej liczbą naturalną.
 
-
 while True:
     try:
-        x = int(input("Prosze podac liczbe naturalna:"))
-        if x >= 0:
+        a = int(input("Ile liczb chcialbys podac?"))
+        if a > 0:
             break
+        else:
+            print("To musi byc liczba naturalna!")
     except:
         print("To musi byc liczba naturalna!")
 
-b = True
+i = 0
+global x
+global flag
 
-for n in range(1, x + 1):
-    # print("n", n)
-    A = n * n + n + 1
-    # print ("A", A)
-    if x == A:
+while i < a:
+    n = 1
+    flag = True
+    b = True
+
+    while True:
+        try:
+            x = int(input("Prosze podac liczbe naturalna:"))
+            if x >= 0:
+                break
+            else:
+                print("To musi byc liczba naturalna!")
+        except:
+            print("To musi byc liczba naturalna!")
+
+    while (flag == True) and (n < x + 1):
+        # print("n", n)
+        A = n * n + n + 1
+        # print("A", A)
+        if x == A:
+            flag = False
+            break
+        if x % A == 0:
+            # print("Mamy dzielnik", A)
+            tmp = x  # Dodajemy pomocnicza zmienna, zeby  w razie czego moc powrocic do pierwotnej petli for
+            while b:
+                # print("x", tmp)
+                tmp = tmp / A
+                # print("po x", tmp)
+                if tmp == 1:  # Jesli liczba dzieli sie przez dany dzielnik i daje 1, to wiemy ze jest wielokrotnoscia.
+                    flag = False
+                    break
+                if (tmp - round(
+                        tmp)) != 0:  # Jesli liczba jest zmiennoprzecinkowa, konczymy petle. Liczenie nie ma dalszego sensu, poniewaz wiemy ze nie bedzie wielokrotnoscia danego dzielnika.
+                    # print(x-round(x))
+                    b = False
+        n += 1
+    if flag == True:
+        print("Nie znaleziono wielokrotnosci")
+    else:
         print("Znaleziono wielokrotnosc")
-        exit()
-    if x % A == 0:
-        print("Mamy dzielnik", A)
-        tmp = x  # Dodajemy pomocnicza zmienna, zeby  w razie czego moc powrocic do pierwotnej petli for
-        while b:
-            #print("x", tmp)
-            tmp = tmp / A
-            #print("po x", tmp)
-            if tmp == 1:  # Jesli liczba dzieli sie przez dany dzielnik i daje 1, to wiemy ze jest wielokrotnoscia.
-                print("Znaleziono wielokrotność")
-                exit()
-            if (tmp - round(tmp)) != 0:  # Jesli liczba jest zmiennoprzecinkowa, konczymy petle. Liczenie nie ma dalszego sensu, poniewaz wiemy ze nie bedzie wielokrotnoscia danego dzielnika.
-                # print(x-round(x))
-                b = False
 
-print("Nie znaleziono wielokrotnosci")
+i += 1
